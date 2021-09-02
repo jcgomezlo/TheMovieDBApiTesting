e2e5cf15faebce0e6008100ddfd4cba79b4933db
jcgomezlo <juancamilo.gomez@endava.com>
2021-09-02 16:46:29 -0500

    Credentials error

--- src/main/java/utils/RouteInstance.java
+++ src/main/java/utils/RouteInstance.java
@@ -10,7 +10,8 @@ public class RouteInstance {
         if(route == null){
             JSONReader<Route> routeJSONReader = new JSONReader<>(Route.class);
             route = routeJSONReader.readJson("data/paths/paths.json");
-            route.setApiKey(System.getenv("API_KEY_MOVIEDB"));
+//            route.setApiKey(System.getenv("API_KEY_MOVIEDB"));
+            route.setApiKey("70e5d92b560911960f954e6433e8daa1");
         }
         return route;
     }

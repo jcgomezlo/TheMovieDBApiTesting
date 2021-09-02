b6678d12bc0fa91e155ed4bd1dd7043c7b988270
jcgomezlo <juancamilo.gomez@endava.com>
2021-09-02 16:56:40 -0500

    Test Github

--- src/main/java/utils/RouteInstance.java
+++ src/main/java/utils/RouteInstance.java
@@ -11,7 +11,7 @@ public class RouteInstance {
             JSONReader<Route> routeJSONReader = new JSONReader<>(Route.class);
             route = routeJSONReader.readJson("data/paths/paths.json");
 //            route.setApiKey(System.getenv("API_KEY_MOVIEDB"));
-            route.setApiKey("70e5d92b560911960f954e6433e8daa1");
+            route.setApiKey("?api_key=70e5d92b560911960f954e6433e8daa1");
         }
         return route;
     }
